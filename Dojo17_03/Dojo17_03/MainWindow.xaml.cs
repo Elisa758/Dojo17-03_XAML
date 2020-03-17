5d99735b8a42fb2cdb5b8e8324fa320642a4b035
Jean <jean.gus.barthelemy@gmail.com>
2020-03-17 11:06:05 +0100

    first commit

--- Dojo17_03/Dojo17_03/MainWindow.xaml.cs
+++ Dojo17_03/Dojo17_03/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
 
 namespace Dojo17_03
 {
@@ -23,6 +25,37 @@ namespace Dojo17_03
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MenuItem_NewFile_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (txtBox.Text != String.Empty )
+            {
+                txtBox.Text = String.Empty;
+            }
+        }
+
+        private void MenuItem_OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            { 
+                txtBox.Text = File.ReadAllText(openFileDialog.FileName);
+               
+            }
+            //openFileDialog.CheckFileExists = false;
+            //openFileDialog.CheckPathExists = false;
+        }
+
+        private void MenuItem_SaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, txtBox.Text);
+            }
+
         }
     }
 }
